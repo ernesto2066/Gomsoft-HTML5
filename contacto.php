9e7f29a93c5de51a6f3f5ca44f4f05737abf052c
Jorge Ernesto Gomez Giraldo <ernesto2066@gmail.com>
2023-01-23 14:57:48 -0500

    Update contact form

--- contacto.php
+++ contacto.php
@@ -1,35 +1,26 @@
 <?php
-
-header ("Location: https://ernesto2066.github.io/gomsoft/");
-//Importamos las variables del formulario de contacto
-
-@$nombre = addslashes($_POST['name']);
-@$email = addslashes($_POST['email']);
-@$asunto = addslashes($_POST['website']);
-@$mensaje = addslashes($_POST['message']);
-
-
-
-//Preparamos el mensaje de contacto
-$cabeceras = "From: $email\n" //La persona que envia el correo
- . "Reply-To: $email\n";
-$asunto = "Mensaje desde la pagina Web"; //asunto aparecera en la bandeja del servidor de correo
-$email_to = "ernesto2066@gmail.com"; //cambiar por tu email
-$contenido = "$nombre ha enviado un mensaje desde la web www.gomsoft.co\n"
-. "\n"
-. "Nombre: $nombre\n"
-. "Email: $email\n"
-. "Sitio Web: $web\n"
-. "Mensaje: $mensaje\n"
-. "\n";
-//Enviamos el mensaje y comprobamos el resultado
-@mail($email_to, $asunto ,$contenido ,$cabeceras ) 
+// Guardar los datos recibidos en variables:
+$nombre = $_POST['name'];
+$email = $_POST['email'];
+$asunto = $_POST['website'];
+$mensaje = $_POST['message'];
+// Definir la dirección de destino:
+$dest = "ernesto2066@gmail.com"; 
  
+// Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
+$headers = "From: $nombre $email\r\n";
+$headers .= "X-Mailer: PHP5\n";
+$headers .= 'MIME-Version: 1.0' . "\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Aqui definimos el asunto y armamos el cuerpo del mensaje
+$cuerpo = "Nombre: ".$nombre."<br>";
+$cuerpo .= "Email: ".$email."<br>";
+$cuerpo .= "Asunto: ".$asunto."<br>";
+$cuerpo .= "Mensaje: ".$mensaje;
 
-//Si el mensaje se envía muestra una confirmación
-die("<p>Gracias, su mensaje se envio correctamente.<p><a>https://ernesto2066.github.io/gomsoft/</a>");
-}else{
-//Si el mensaje no se envía muestra el mensaje de error
-die("Error: Su información no pudo ser enviada, intente más tarde");
+// Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido:
+if($nombre != '' && $email != '' && $asunto != '' && $mensaje != ''){
+    mail($dest,$asunto,$cuerpo,$headers); //ENVIAR!
 }
 ?>
