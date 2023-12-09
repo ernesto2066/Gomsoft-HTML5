0bf8cd8a8550d8e8c04cbc5fe2157919248c6e09
Jorge Ernesto Gomez Giraldo <ernesto2066@gmail.com>
2023-12-09 15:06:47 -0500

    Cambios finalizacion del año 2023 al formulario de contacto

--- contacto.php
+++ contacto.php
@@ -1,5 +1,5 @@
 <?php
-
+#Metodo para el envio de correos desde la pagina web
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     # Guardar los datos recibidos en variables validando el metodo post de ingreso de datos del formulario html
     $nombre = $_POST['name'];
@@ -22,42 +22,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<p>El campo de mensaje es obligatorio</p>";
         exit;
     }
-    # Definir la dirección de destino:
+    # Definir la dirección de destino de gomsoft
     $dest = "jorge@gomsoft.co"; 
     $mensaje_correo = "Nombre: $nombre\nCorreo electrónico: $correo\nAsunto: $asunto\nMensaje:\n$mensaje";
     $cabeceras = "From: $correo\r\n";
     $cabeceras .= "Reply-To: $correo\r\n";
 
     if (mail($dest, $asunto, $mensaje_correo, $cabeceras)) {
-        # code...
+        # Validacion para confirmar el envio de correo electronico de manera correcta
         echo "<p>El mensaje ha sido enviado correctamente</p>";
     } else {
-        # code...
+        # Confirmacion si encuentra algun error en el envio de correo
         echo "<p>Ha habido un error al enviar el mensaje</p>";
     }
-
-
 } else {
-    # code...
+    # Confirmacion si encuentra algun error en el envio de correo
     header("Location: index.html");
     echo "<p>Algo salió mal en el envió del correo</p>";
     exit;
 }
-/*
-// Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
-$headers = "From: $nombre $correo\r\n";
-$headers .= "X-Mailer: PHP5\n";
-$headers .= 'MIME-Version: 1.0' . "\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
- 
-// Aqui definimos el asunto y armamos el cuerpo del mensaje
-$cuerpo = "Nombre: ".$nombre."<br>";
-$cuerpo .= "Email: ".$correo."<br>";
-$cuerpo .= "Asunto: ".$asunto."<br>";
-$cuerpo .= "Mensaje: ".$mensaje;
-
-// Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido:
-if($nombre != '' && $correo != '' && $asunto != '' && $mensaje != ''){
-    mail($dest,$asunto,$cuerpo,$headers); //ENVIAR!
-}*/
 ?>
